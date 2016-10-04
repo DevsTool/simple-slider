673c7605fe0319c154de7b54f5d08afbeb62869d
photon <photon@templatemonster.me>
2016-10-04 13:42:18 +0300

    FIX: https://github.com/kaskad88/simple-slider/issues/1 FIX: https://github.com/kaskad88/simple-slider/issues/12

--- js/jquery.simpleSlider.js
+++ js/jquery.simpleSlider.js
@@ -2,273 +2,279 @@
  * Simple slider.
  */
 (function( $ ) {
-    'use strict';
+	'use strict';
 
-    var methods = {
+	var methods = {
 
-        init : function( options ) {
-            var settings = {
-                height: 500,
-                animation: 'slide',
-                duration: 1500,
-                navigation: true,
-                navigationPrev: 'Prev',
-                navigationNext: 'Next',
-                pagination: true,
-                autoPlay: false,
-                autoPlayDelay: 3000,
-                jsonData: false,
-                jsonPatch: ''
-            };
+		init: function( options ) {
+			var settings = {
+				height: 500,
+				animation: 'slide',
+				duration: 1500,
+				navigation: true,
+				navigationPrev: 'Prev',
+				navigationNext: 'Next',
+				pagination: true,
+				autoPlay: false,
+				autoPlayDelay: 10000,
+				jsonData: false,
+				jsonPatch: ''
+			};
 
-            return this.each( function() {
-                if ( options ) {
-                    $.extend( settings, options );
-                }
+			return this.each( function() {
+				if ( options ) {
+					$.extend( settings, options );
+				}
 
-                var $this             = $( this ),
-                    $sliderWrap       = $( '.simple-slider__wrap', $this ),
-                    $sliderItems      = $( '.simple-slider__items', $this ),
-                    $sliderItem       = $( '.simple-slider__item', $sliderItems ),
-                    $itemsCount       = $sliderItem.length,
-                    $sliderItemsWidth = $sliderItems.width(),
-                    $indexCurrentItem = 0;
+				var $this = $( this ),
+					$sliderWrap = $( '.simple-slider__wrap', $this ),
+					$sliderItems = $( '.simple-slider__items', $this ),
+					$sliderItem = $( '.simple-slider__item', $sliderItems ),
+					$itemsCount = $sliderItem.length,
+					$sliderItemsWidth = $sliderItems.width(),
+					$indexCurrentItem = 0;
 
-                ( function () {
+				(function() {
 
-                    if ( settings.jsonData ) {
-                        buildJsonItems();
-                    }
+					if ( settings.jsonData ) {
+						buildJsonItems();
+					}
 
-                    sliderStyle();
+					sliderStyle();
 
-                    if ( settings.navigation ) {
-                        buildNavigationHTML();
-                        addNavigationEvents();
-                    }
+					if ( settings.navigation ) {
+						buildNavigationHTML();
+						addNavigationEvents();
+					}
 
-                    if ( settings.pagination ) {
-                        buildPaginationHTML();
-                        addPaginationEvents();
-                    }
-                    if ( settings.autoPlay ) {
-                        autoPlay();
-                    }
+					if ( settings.pagination ) {
+						buildPaginationHTML();
+						addPaginationEvents();
+					}
+					if ( settings.autoPlay ) {
+						autoPlay();
+					}
 
-                    addResizeEvent();
+					addResizeEvent();
 
-                } )();
+				})();
 
 
-                /**
-                 * Slider inline style.
-                 */
-                function sliderStyle() {
-                    $sliderItems.css({
-                        'height' : settings.height,
-                        'animationDuration':  settings.duration + 'ms',
-                        'transitionDuration':  settings.duration + 'ms'
-                    });
+				/**
+				 * Slider inline style.
+				 */
+				function sliderStyle() {
+					$sliderItems.css( {
+						'height': settings.height,
+						'animationDuration': settings.duration + 'ms',
+						'transitionDuration': settings.duration + 'ms'
+					} );
 
-                    $sliderItem.width( $sliderItemsWidth );
+					$sliderItem.width( $sliderItemsWidth );
 
-                    $this.addClass( 'simple-slider--' + settings.animation );
-                    $sliderItem.first()
-                               .addClass( 'current' );
-                }
+					$this.addClass( 'simple-slider--' + settings.animation );
+					$sliderItem.first()
+						.addClass( 'current' );
+				}
 
-                /**
-                 * Build Navigation Html.
-                 */
-                function buildNavigationHTML() {
-                    var $next = '<div class="simple-slider__next">' + settings.navigationNext + '</div>',
-                        $prev = '<div class="simple-slider__prev">' + settings.navigationPrev + '</div>';
+				/**
+				 * Build Navigation Html.
+				 */
+				function buildNavigationHTML() {
+					var $next = '<div class="simple-slider__next">' + settings.navigationNext + '</div>',
+						$prev = '<div class="simple-slider__prev">' + settings.navigationPrev + '</div>';
 
-                    $sliderWrap.append( '<div class="simple-slider__navigation">' + $prev + $next + '</div>' );
-                }
+					$sliderWrap.append( '<div class="simple-slider__navigation">' + $prev + $next + '</div>' );
+				}
 
-                /**
-                 * Build Pagination Html.
-                 */
-                function buildPaginationHTML(){
-                    var bulletHtml = '<span class="simple-slider__bullet"></span>',
-                        i          = 1;
+				/**
+				 * Build Pagination Html.
+				 */
+				function buildPaginationHTML() {
+					var bulletHtml = '<span class="simple-slider__bullet"></span>',
+						i = 1;
 
-                   $this.append( '<div class="simple-slider__pagination"></div>' );
-                   for ( ; i <= $itemsCount; i++ ) {
-                       $( '.simple-slider__pagination', $this ).append( bulletHtml );
-                   }
-                   $( '.simple-slider__bullet', $this )
-                       .first()
-                       .addClass( 'active' );
-                }
+					$this.append( '<div class="simple-slider__pagination"></div>' );
+					for ( ; i <= $itemsCount; i++ ) {
+						$( '.simple-slider__pagination', $this ).append( bulletHtml );
+					}
+					$( '.simple-slider__bullet', $this )
+						.first()
+						.addClass( 'active' );
+				}
 
-                /**
-                 * Add slider navigation events.
-                 */
-                function addNavigationEvents() {
-                    var $nextBtn = $( '.simple-slider__next', $this ),
-                        $prevBtn = $( '.simple-slider__prev', $this );
+				/**
+				 * Add slider navigation events.
+				 */
+				function addNavigationEvents() {
+					var $nextBtn = $( '.simple-slider__next', $this ),
+						$prevBtn = $( '.simple-slider__prev', $this );
 
-                    $nextBtn.on( 'click', slidingNext );
-                    $prevBtn.on( 'click', slidingPrev );
-                }
+					$nextBtn.on( 'click', slidingNext );
+					$prevBtn.on( 'click', slidingPrev );
+				}
 
-                /**
-                 * Add slider pagination events.
-                 */
-                function addPaginationEvents() {
-                    var $bullet  = $( '.simple-slider__bullet', $this );
+				/**
+				 * Add slider pagination events.
+				 */
+				function addPaginationEvents() {
+					var $bullet = $( '.simple-slider__bullet', $this );
 
-                    $bullet.on( 'click', slidingBullet );
-                }
+					$bullet.on( 'click', slidingBullet );
+				}
 
-                /**
-                 * Sliding Next Handler.
-                 */
-                function slidingNext() {
-                    var $offset;
+				/**
+				 * Sliding Next Handler.
+				 */
+				function slidingNext() {
+					var $offset;
 
-                    if ( $indexCurrentItem === ( $itemsCount - 1 ) ) {
-                        $offset = 0;
-                        $indexCurrentItem = 0;
+					if ( $indexCurrentItem === ( $itemsCount - 1 ) ) {
+						$offset = 0;
+						$indexCurrentItem = 0;
 
-                    } else {
-                        $offset = $sliderItemsWidth * ( $indexCurrentItem + 1 );
-                        $indexCurrentItem++;
-                    }
+					}
+					else {
+						$offset = $sliderItemsWidth * ( $indexCurrentItem + 1 );
+						$indexCurrentItem++;
+					}
 
-                    slidingAnimation( $offset );
-                    addActiveClasses();
-                }
+					slidingAnimation( $offset );
+					addActiveClasses();
+				}
 
-                /**
-                 * Sliding Prev Handler.
-                 */
-                function slidingPrev() {
-                    var $offset;
+				/**
+				 * Sliding Prev Handler.
+				 */
+				function slidingPrev() {
+					var $offset;
 
-                    if ( $indexCurrentItem === 0 ) {
-                        $offset = $sliderItemsWidth * ( $itemsCount - 1 );
-                        $indexCurrentItem = $itemsCount - 1;
-                    } else {
-                        $offset = ( ( $sliderItemsWidth * $indexCurrentItem ) - $sliderItemsWidth );
-                        $indexCurrentItem--;
-                    }
+					if ( $indexCurrentItem === 0 ) {
+						$offset = $sliderItemsWidth * ( $itemsCount - 1 );
+						$indexCurrentItem = $itemsCount - 1;
+					}
+					else {
+						$offset = ( ( $sliderItemsWidth * $indexCurrentItem ) - $sliderItemsWidth );
+						$indexCurrentItem--;
+					}
 
-                    slidingAnimation( $offset );
-                    addActiveClasses();
-                }
+					slidingAnimation( $offset );
+					addActiveClasses();
+				}
 
-                /**
-                 * Sliding Bullet Handler.
-                 */
-                function slidingBullet() {
-                    var $bulletIndex = $( this ).index(),
-                        $offset;
+				/**
+				 * Sliding Bullet Handler.
+				 */
+				function slidingBullet() {
+					var $bulletIndex = $( this ).index(),
+						$offset;
 
-                    $indexCurrentItem = $bulletIndex;
-                    $offset           = $sliderItemsWidth * $indexCurrentItem ;
+					$indexCurrentItem = $bulletIndex;
+					$offset = $sliderItemsWidth * $indexCurrentItem;
 
-                    slidingAnimation( $offset );
-                    addActiveClasses();
-                }
+					slidingAnimation( $offset );
+					addActiveClasses();
+				}
 
-                /**
-                 * Sliding animation.
-                 */
-                function slidingAnimation( $offset ) {
-                    $sliderItems.css({
-                        '-webkit-transform': 'translate3d(' + -$offset + 'px, 0px, 0px)',
-                        '-moz-transform': 'translate3d(' + -$offset + 'px, 0px, 0px)',
-                        '-o-transform': 'translate3d(' + -$offset + 'px, 0px, 0px)',
-                        '-ms-transform': 'translate3d(' + -$offset + 'px, 0px, 0px)',
-                        'transform': 'translate3d(' + -$offset + 'px, 0px,0px)'
-                    });
-                }
+				/**
+				 * Sliding animation.
+				 */
+				function slidingAnimation( $offset ) {
+					var $cssTranslate3d = 'translate3d(' + -$offset + 'px, 0px, 0px)';
 
-                /**
-                 * Add active classes.
-                 */
-                function addActiveClasses() {
-                    var $activeBullet     = $( '.simple-slider__bullet.active', $this ),
-                        $bullet           = $( '.simple-slider__bullet', $this),
-                        $activeSliderItem = $( '.simple-slider__item.current', $sliderItems );
+					$sliderItems.css( {
+						'-webkit-transform': $cssTranslate3d,
+						'-moz-transform': $cssTranslate3d,
+						'-o-transform': $cssTranslate3d,
+						'-ms-transform': $cssTranslate3d,
+						'transform': $cssTranslate3d
+					} );
+				}
 
-                    // add active class to bullet
-                    $activeBullet.removeClass( 'active' );
-                    $bullet.eq( $indexCurrentItem ).addClass( 'active' );
+				/**
+				 * Add active classes.
+				 */
+				function addActiveClasses() {
+					var $activeBullet = $( '.simple-slider__bullet.active', $this ),
+						$bullet = $( '.simple-slider__bullet', $this ),
+						$activeSliderItem = $( '.simple-slider__item.current', $sliderItems );
 
-                    // add active class to current slider item
-                    $activeSliderItem.removeClass( 'current' );
-                    $sliderItem.eq( $indexCurrentItem ).addClass( 'current' );
+					// add active class to bullet
+					$activeBullet.removeClass( 'active' );
+					$bullet.eq( $indexCurrentItem ).addClass( 'active' );
 
-                    // add animation class
-                    $sliderItems.addClass( settings.animation );
-                    setTimeout( function() {
-                        $sliderItems.removeClass( settings.animation );
-                    }, settings.duration );
+					// add active class to current slider item
+					$activeSliderItem.removeClass( 'current' );
+					$sliderItem.eq( $indexCurrentItem ).addClass( 'current' );
 
-                }
+					// add animation class
+					$sliderItems.addClass( settings.animation );
+					setTimeout( function() {
+						$sliderItems.removeClass( settings.animation );
+					}, settings.duration );
 
-                /**
-                 * Auto play.
-                 */
-                function autoPlay(){
-                    setInterval( function() {
-                        slidingNext();
-                    }, settings.autoPlayDelay );
-                }
+				}
 
-                /**
-                 * Add resize event.
-                 */
-                function addResizeEvent() {
-                    $( window ).on( 'resize.simple_slider', function() {
-                        $sliderItemsWidth = $sliderItems.width();
-                        $sliderItem.width( $sliderItemsWidth );
-                    } );
-                }
+				/**
+				 * Auto play.
+				 */
+				function autoPlay() {
+					setInterval( function() {
+						slidingNext();
+					}, settings.autoPlayDelay );
+				}
 
-                /**
-                 * Build json items.
-                 */
-                function buildJsonItems() {
-                    if ( ! settings.jsonPatch ) {
-                        return false;
-                    }
+				/**
+				 * Add resize event.
+				 */
+				function addResizeEvent() {
+					$( window ).on( 'resize.simple_slider', function() {
+						$sliderItemsWidth = $sliderItems.width();
+						$sliderItem.width( $sliderItemsWidth );
+					} );
+				}
 
-                    $.getJSON(
-                        settings.jsonPatch,
-                        function ( data ) {
-                            var $sliderItemHtml = '';
+				/**
+				 * Build json items.
+				 */
+				function buildJsonItems() {
+					if ( !settings.jsonPatch ) {
+						return false;
+					}
 
-                            for( var i in data['items'] ) {
+					$.getJSON(
+						settings.jsonPatch,
+						function( data ) {
+							var $sliderItemHtml = '';
 
-                                var $img = data['items'][i].img,
-                                    $alt = data['items'][i].alt;
+							for ( var i in data['items'] ) {
 
-                                $sliderItemHtml += '<div class="simple-slider__item"><img src="' + $img + '" alt="' + $alt + '"></div>';
-                            }
-                            $sliderItems.append( $sliderItemHtml );
+								var $img = data['items'][i].img,
+									$alt = data['items'][i].alt;
 
-                        }
-                    );
-                }
+								$sliderItemHtml += '<div class="simple-slider__item"><img src="' + $img + '" alt="' + $alt + '"></div>';
+							}
+							$sliderItems.append( $sliderItemHtml );
 
-            });
-        }
-    };
+						}
+					);
+				}
 
-    $.fn.simpleSlider = function( method ) {
+			} );
+		}
+	};
 
-        if ( methods[method] ) {
-            return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ) );
-        } else if ( typeof method === 'object' || ! method ) {
-            return methods.init.apply( this, arguments );
-        } else {
-            $.error( 'Method with name ' + method + ' is not exist for simpleSlider jQuery plugin' );
-        }
+	$.fn.simpleSlider = function( method ) {
 
-    };
-})(jQuery);
+		if ( methods[method] ) {
+			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ) );
+		}
+		else if ( typeof method === 'object' || !method ) {
+			return methods.init.apply( this, arguments );
+		}
+		else {
+			$.error( 'Method with name ' + method + ' is not exist for simpleSlider jQuery plugin' );
+		}
+
+	};
+})( jQuery );
--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
 /**
  * Scripts init.
  */
-(function ($) {
-    "use strict";
+(function( $ ) {
+	"use strict";
 
-    $( document ).ready( function () {
-        var $slider = $( '.simple-slider' );
-        if ( $slider.length > 0 ) {
-            $slider.each( function() {
-                var $this = $( this ),
-                    $sliderHeight     = $this.data( 'height' ),
-                    $sliderDuration   = $this.data( 'duration' ),
-                    $sliderAutoPlay   = $this.data( 'auto-play' ),
-                    $sliderAnimation  = $this.data( 'animation' ),
-                    $sliderNavigation = $this.data( 'navigation' ),
-                    $sliderPagination = $this.data( 'pagination' ),
-                    $sliderJson       = $this.data( 'json'),
-                    $sliderJsonPath   = $this.data( 'json-path' );
+	$( document ).ready( function() {
+		var $slider = $( '.simple-slider' );
+		if ( $slider.length > 0 ) {
+			$slider.each( function() {
+				var $this = $( this ),
+					$sliderHeight = $this.data( 'height' ),
+					$sliderDuration = $this.data( 'duration' ),
+					$sliderAutoPlay = $this.data( 'auto-play' ),
+					$sliderAnimation = $this.data( 'animation' ),
+					$sliderNavigation = $this.data( 'navigation' ),
+					$sliderPagination = $this.data( 'pagination' ),
+					$sliderJson = $this.data( 'json' ),
+					$sliderJsonPath = $this.data( 'json-path' );
 
-                $this.simpleSlider({
-                    height:         $sliderHeight,
-                    duration:       $sliderDuration,
-                    navigation:     $sliderNavigation,
-                    pagination:     $sliderPagination,
-                    animation:      $sliderAnimation,
-                    navigationPrev: '<i class="fa fa-chevron-left"></i>',
-                    navigationNext: '<i class="fa fa-chevron-right"></i>',
-                    autoPlay:       $sliderAutoPlay,
-                    jsonData:       $sliderJson,
-                    jsonPatch:      $sliderJsonPath
-                });
-            });
+				$this.simpleSlider( {
+					height: $sliderHeight,
+					duration: $sliderDuration,
+					navigation: $sliderNavigation,
+					pagination: $sliderPagination,
+					animation: $sliderAnimation,
+					navigationPrev: '<i class="fa fa-chevron-left"></i>',
+					navigationNext: '<i class="fa fa-chevron-right"></i>',
+					autoPlay: $sliderAutoPlay,
+					jsonData: $sliderJson,
+					jsonPatch: $sliderJsonPath
+				} );
+			} );
 
-            // Default init
-            //$slider.simpleSlider();
-        }
-    });
+			// Default init
+			//$slider.simpleSlider();
+		}
+	} );
 })( jQuery );
