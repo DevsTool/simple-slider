273fd93ed7fbebbeb5677db175d61f35a18861fe
photon <photon@templatemonster.me>
2016-10-13 20:31:07 +0300

    FIX: https://github.com/kaskad88/simple-slider/issues/2 FIX: https://github.com/kaskad88/simple-slider/issues/3 FIX: https://github.com/kaskad88/simple-slider/issues/4

--- js/script.js
+++ js/script.js
@@ -4,37 +4,36 @@
 (function( $ ) {
 	"use strict";
 
-	$( document ).ready( function() {
-		var $slider = $( '.simple-slider' );
-		if ( $slider.length > 0 ) {
-			$slider.each( function() {
-				var $this = $( this ),
-					settings = $this.data( 'settings' ),
-					$sliderHeight = settings['height'],
-					$sliderDuration = settings['duration'],
-					$sliderAutoPlay = settings['auto-play'],
-					$sliderAnimation = settings['animation'],
-					$sliderNavigation = settings['navigation'],
-					$sliderPagination = settings['pagination'],
-					$sliderJson = settings['json'],
-					$sliderJsonPath = settings['json-path'];
+	var $slider = $( '.simple-slider' );
+	if ( $slider.length > 0 ) {
+		$slider.each( function() {
+			var $this = $( this ),
+				settings = $this.data( 'settings' ),
+				$sliderHeight = settings['height'],
+				$sliderDuration = settings['duration'],
+				$sliderAutoPlay = settings['auto-play'],
+				$sliderAnimation = settings['animation'],
+				$sliderNavigation = settings['navigation'],
+				$sliderPagination = settings['pagination'],
+				$sliderJson = settings['json'],
+				$sliderJsonPath = settings['json-path'];
 
-				$this.simpleSlider( {
-					height: $sliderHeight,
-					duration: $sliderDuration,
-					navigation: $sliderNavigation,
-					pagination: $sliderPagination,
-					animation: $sliderAnimation,
-					navigationPrev: '<i class="fa fa-chevron-left"></i>',
-					navigationNext: '<i class="fa fa-chevron-right"></i>',
-					autoPlay: $sliderAutoPlay,
-					jsonData: $sliderJson,
-					jsonPatch: $sliderJsonPath
-				} );
+			$this.simpleSlider( {
+				height: $sliderHeight,
+				duration: $sliderDuration,
+				navigation: $sliderNavigation,
+				pagination: $sliderPagination,
+				animation: $sliderAnimation,
+				navigationPrev: '<i class="fa fa-chevron-left"></i>',
+				navigationNext: '<i class="fa fa-chevron-right"></i>',
+				autoPlay: $sliderAutoPlay,
+				jsonData: $sliderJson,
+				jsonPatch: $sliderJsonPath
 			} );
+		} );
 
-			// Default init
-			//$slider.simpleSlider();
-		}
-	} );
+		// Default init
+		//$slider.simpleSlider();
+	}
+
 })( jQuery );
